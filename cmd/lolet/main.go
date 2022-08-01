779827aaad3fdcc43cb95ba31a023107b060c5a1
Cole <cole1001@qq.com>
2022-08-01 22:48:47 +0800

    Add the function to parse the players csv data

--- cmd/lolet/main.go
+++ cmd/lolet/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gocarina/gocsv"
+	"os"
+)
+
+type Player struct {
+	Name   string `csv:"name"`
+	Lane   string `csv:"lane"`
+	Region string `csv:"region"`
+	Team   string `csv:"team"`
+	Tag1   string `csv:"tag1"`
+	Tag2   string `csv:"tag2"`
+	Level  string `csv:"level"`
+}
 
 func main() {
 	fmt.Println("hello world")
+	players, err := loadPlayers()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, player := range players {
+		fmt.Printf("%#v\n", player)
+	}
+}
+
+func loadPlayers() ([]Player, error) {
+	playersFile, err := os.OpenFile("data/players.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	defer playersFile.Close()
+
+	players := make([]Player, 0)
+	if err = gocsv.UnmarshalFile(playersFile, &players); err != nil {
+		return nil, err
+	}
+
+	return players, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hkail/lolet
 
 go 1.18
+
+require github.com/gocarina/gocsv v0.0.0-20220729221910-a7386ae0b221 // indirect
